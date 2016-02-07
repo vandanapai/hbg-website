dccf09ac67d507b57372de45bd123102d739fca9
Vandana Pai <vandanapai01@gmail.com>
2016-02-07 21:32:47 +0700

    navigation is now responsive, added mobile button

--- js/main.js
+++ js/main.js
@@ -21,15 +21,13 @@ $(document).ready(function () {
 	});
 
     $('div[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
+        var $bgobj = $(this);
     
         $(window).scroll(function() {
             var yPos = -($(window).scrollTop() / $bgobj.data('speed')); 
             
-            // Put together our final background position
             var coords = '50% '+ yPos + 'px';
 
-            // Move the background
             $bgobj.css({ backgroundPosition: coords });
         }); 
     });  
